f48588035ab5c5fb0f7479c970e5bbc40019d8bc
Daniel <daniel.poulsen@live.dk>
2020-10-20 16:28:23 +0200

    Exceptions

---
+++ src/main/java/exceptions/PersonNotFoundException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+/**
+ *
+ * @author Dane
+ */
+public class PersonNotFoundException extends Exception {
+
+    public PersonNotFoundException(String message) {
+        super(message);
+    }
+}
