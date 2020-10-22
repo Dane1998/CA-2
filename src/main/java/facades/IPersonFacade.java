91dc422776129fc69217c1ac37b3d5038fe75bbb
Daniel <daniel.poulsen@live.dk>
2020-10-22 11:06:13 +0200

    exception

--- src/main/java/facades/IPersonFacade.java
+++ src/main/java/facades/IPersonFacade.java
@@ -5,6 +5,7 @@ import dto.PersonDTO;
 import dto.PersonsDTO;
 import entities.CityInfo;
 import entities.Hobby;
+import exceptions.PersonNotFoundException;
 
 /**
  *
@@ -13,7 +14,7 @@ import entities.Hobby;
 public interface IPersonFacade {
     
     public PersonsDTO getAllPersons();
-    public PersonDTO getPersonById(long id);
+    public PersonDTO getPersonById(long id)throws PersonNotFoundException;
     public PersonDTO getPersonByPhone(int number);
     public int getPersoncountByHobby(Hobby hobby);
     public PersonDTO getPersonsByHobby(Hobby hobby);
